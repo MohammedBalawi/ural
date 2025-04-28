360a50aa08de4188fca63130f2ef8aa572aa45c2
Mohammed Balawi <bbmm6066@gmail.com>
2025-04-28 04:08:53 +0300

    🔧 Add GitHub Actions workflow to build APK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.helps"
-    namespace 'com.flutter.notificationlistener'
+    namespace ='com.flutter.notificationlistener'
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
